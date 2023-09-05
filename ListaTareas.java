430751e599291facfc8c14c72063332e794238ce
DiegoCorralHdz <143724924+DiegoCorralHdz@users.noreply.github.com>
2023-09-05 09:46:45 -0500

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author labcca
+ */
+public class ListaTareas {
+   public static void main(String[] args) {
+   
+       
+       String tarea1 = "lavar los trastes";
+       String tarea2= "limpiar mi cuarto";
+       String tarea3 = "Pasear a la mascota";
+       String tarea4 = "Hacer la comida";
+       String tarea5= "Estudiar";
+       
+       System.out.println("Primer tarea: "+ tarea1+" Segunda tarea: "  + tarea2+" Tercer tarea : "+ tarea3+ " Cuarta tarea: " + tarea4 +" Quinta tarea: "+ tarea5 );
+   }}
