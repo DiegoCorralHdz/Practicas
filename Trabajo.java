13032c858bfcb7fe33f429cb7754da3fd0bc7d13
DiegoCorralHdz <143724924+DiegoCorralHdz@users.noreply.github.com>
2023-09-07 09:43:41 -0500

    Add files via upload

---
+++ Trabajo.java
@@ -0,0 +1,16 @@
+
+package trabajo;
+
+
+public class Trabajo {
+
+    public static void main(String[] args) {
+         int base = 10;
+      int altura = 20;
+      String nombre = "Diego Adrian Corral Hernandez";
+      int arearectangulo = base*altura;
+        System.out.println("lA ALTURA DEL RECTANGULO ES: "+ arearectangulo);
+        System.out.println("Mi nombre es: "+ nombre);
+    }
+    
+}
